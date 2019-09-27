0136ea672a212b393c5bc5bb098a0a3cf1d358aa
Saahit K <saahit.karumuri.977@k12.friscoisd.org>
2019-09-27 11:06:18 -0500

    Player 1 & 2 difference

--- BigPigGame/ViewController.swift
+++ BigPigGame/ViewController.swift
@@ -17,17 +17,25 @@ class ViewController: UIViewController {
 
    
     @IBOutlet var labelForDice: UILabel!
-    //
-    
+    @IBOutlet var labelNumber: UILabel!
+    var play1or2: Bool = true
     var totalNumber = 0
+    //
     @IBAction func buttonPress(_ sender: Any) {
+        labelNumber.text = "Player 1"
         let randomInt = Int.random(in: 1...6)
         if randomInt == 1 {
             labelForDice.text = "You rolled a \(randomInt)! Unforunately, you busted, but your total score, \(totalNumber), is unchanged."
+            labelNumber.text = "Player 2"
+            
         } else {
             labelForDice.text = "You rolled a \(randomInt)!"
             totalNumber = randomInt + totalNumber
         }
+        //
+    }
+    @IBAction func nextPlayerTurn(_ sender: Any) {
+        play1or2 = false;
     }
 }
 
