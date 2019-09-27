a317a004d91b91524d0c4f4683f9d06a1802ccf5
Saahit K <saahit.karumuri.977@k12.friscoisd.org>
2019-09-27 11:54:56 -0500

    g

--- BigPigGame/ViewController.swift
+++ BigPigGame/ViewController.swift
@@ -25,23 +25,22 @@ class ViewController: UIViewController {
     @IBAction func buttonPress(_ sender: Any) {
         labelNumber.text = "Player 1"
         let randomInt = Int.random(in: 1...6)
-        while play1or2 == true
+        if play1or2
         {
             if randomInt == 1 {
                 labelForDice.text = "You rolled a \(randomInt)! Unforunately, you busted, but your total score, \(totalNumberPlayer1), is unchanged."
                 labelNumber.text = "Player 2"
-                break
             } else {
                 labelForDice.text = "You rolled a \(randomInt)!"
                 totalNumberPlayer1 = randomInt + totalNumberPlayer1
             }
         }
-        while play1or2 == false
+        if !play1or2
         {
             if randomInt == 1 {
                 labelForDice.text = "You rolled a \(randomInt)! Unforunately, you busted, but your total score, \(totalNumberPlayer2), is unchanged."
                 labelNumber.text = "Player 2"
-                break
+
             } else {
                 labelForDice.text = "You rolled a \(randomInt)!"
                 totalNumberPlayer2 = randomInt + totalNumberPlayer2
@@ -50,7 +49,15 @@ class ViewController: UIViewController {
         //
     }
     @IBAction func nextPlayerTurn(_ sender: Any) {
-        
+        var i = 0
+        if i == 0 {
+            play1or2 = false
+            i = 1
+        }
+        if i == 1 {
+            i = 0
+            play1or2 = true
+        }
     }
 }
 
