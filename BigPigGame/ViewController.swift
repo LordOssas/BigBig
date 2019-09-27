4ce97506a603b63344338928f9215799727a5491
Saahit K <saahit.karumuri.977@k12.friscoisd.org>
2019-09-27 11:13:31 -0500

    loops and total 2

--- BigPigGame/ViewController.swift
+++ BigPigGame/ViewController.swift
@@ -19,18 +19,33 @@ class ViewController: UIViewController {
     @IBOutlet var labelForDice: UILabel!
     @IBOutlet var labelNumber: UILabel!
     var play1or2: Bool = true
-    var totalNumber = 0
+    var totalNumberPlayer1 = 0
+    var totalNumberPlayer2 = 0
     //
     @IBAction func buttonPress(_ sender: Any) {
         labelNumber.text = "Player 1"
         let randomInt = Int.random(in: 1...6)
-        if randomInt == 1 {
-            labelForDice.text = "You rolled a \(randomInt)! Unforunately, you busted, but your total score, \(totalNumber), is unchanged."
-            labelNumber.text = "Player 2"
-            
-        } else {
-            labelForDice.text = "You rolled a \(randomInt)!"
-            totalNumber = randomInt + totalNumber
+        while play1or2 == true
+        {
+            if randomInt == 1 {
+                labelForDice.text = "You rolled a \(randomInt)! Unforunately, you busted, but your total score, \(totalNumberPlayer1), is unchanged."
+                labelNumber.text = "Player 2"
+                break;
+            } else {
+                labelForDice.text = "You rolled a \(randomInt)!"
+                totalNumberPlayer1 = randomInt + totalNumberPlayer1
+            }
+        }
+        while play1or2 == false
+        {
+            if randomInt == 1 {
+                labelForDice.text = "You rolled a \(randomInt)! Unforunately, you busted, but your total score, \(totalNumberPlayer2), is unchanged."
+                labelNumber.text = "Player 2"
+                break;
+            } else {
+                labelForDice.text = "You rolled a \(randomInt)!"
+                totalNumberPlayer2 = randomInt + totalNumberPlayer2
+            }
         }
         //
     }
